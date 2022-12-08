9278e444cc6df128d67b10f2ec9d60527273a87b
sdacruz1 <stephanie_da_cruz@brown.edu>
2022-12-08 14:07:38 -0500

    trying to see deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>HELLO??????</h1>
         <p>
           Hello World, does this work?
         </p>
